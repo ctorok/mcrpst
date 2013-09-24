e3ccc2cd1e6621bfb6f7e5b6a31ab92d1b21bd55
ctorok <ctorok@gmail.com>
2013-09-24 14:57:03 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mcrpst::Application.config.secret_token = '1d95f67489f103d1da176486cd6bf4558a31ffae7cbbf4def2b866d0b653c8b9faa28d0873459c29f151f840bb8956730536f35c951e82779e2baabc980e401c'
