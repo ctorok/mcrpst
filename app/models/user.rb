cdc60335a29fec030fcfbc3eb42d0f4fd56796de
ctorok <ctorok@gmail.com>
2013-09-26 10:57:19 -0400

    add foundation and minimal styling

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,15 @@
 class User < ActiveRecord::Base
+  attr_accessor :name, :email
   has_many :microposts
+
+  def initialize(attributes = {})
+    @name = attributes[:name]
+    @email = attributes[:email]
+  end
+
+  # def format_email
+  #   "#{@name} <#{@email}>"
+  # end
+
+
 end
